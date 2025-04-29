d4b200213bf175a5f0ac5d4e1647908a6b6700d5
Peter Pak <ppak10@gmail.com>
2025-04-29 13:59:54 -0400

    Updates entrypoint with moving the client process to screen.

--- services/ubuntu_22.04/entrypoint.sh
+++ services/ubuntu_22.04/entrypoint.sh
@@ -5,10 +5,11 @@ if [ ! -f /home/app/.devcontainer.json ]; then
   cp /home/ubuntu/.devcontainer.json /home/app/
 fi
 
-. app/venv/bin/activate
-
 # Install app dependencies
 # echo "Installing app requirements"
 # pip install -r requirements.txt
+# source /home/app/venv/bin/activate && python -m app.client.start_services
 
-python -m app.client.start_services
+screen -dmS app bash -c "source /home/app/venv/bin/activate && exec python -m app.client.start_services"
+
+tail -f /dev/null
